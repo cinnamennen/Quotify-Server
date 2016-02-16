e9466624445e7233bf2052f9fecdaab8c7fdad0f
Caleb <calebmennen@icloud.com>
2016-02-15 22:08:10 -0500

    m

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask.ext.httpauth import HTTPBasicAuth
+import os
 
 app = Flask(__name__, static_url_path="")
 auth = HTTPBasicAuth()
@@ -116,4 +117,5 @@ def delete_task(task_id):
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True)
